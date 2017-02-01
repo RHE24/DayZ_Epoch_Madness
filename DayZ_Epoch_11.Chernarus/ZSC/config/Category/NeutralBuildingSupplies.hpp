0ec55bbde70ed3c359ab2ca2195f19c24f0d83d3
Michael Gallagher <mikewizcob@gmail.com>
2017-02-01 02:05:46 -0700

    Updates to Traders and Spwaned vehilces

--- DayZ_Epoch_11.Chernarus/ZSC/config/Category/NeutralBuildingSupplies.hpp
+++ DayZ_Epoch_11.Chernarus/ZSC/config/Category/NeutralBuildingSupplies.hpp
@@ -1,4 +1,4 @@
-class Category_662 {
+﻿class Category_662 {
 	class ItemSandbag {
 		type = "trade_items";
 		buy[] ={4000,"Coins"};
@@ -13,6 +13,11 @@ class Category_662 {
 		type = "trade_items";
 		buy[] ={1200,"Coins"};
 		sell[] ={600,"Coins"};
+	};
+	class ItemPole {
+		type = "trade_items";
+		buy[] ={300,"Coins"};
+		sell[] ={150,"Coins"};
 	};
 	class ItemWire {
 		type = "trade_items";
@@ -48,13 +53,13 @@ class Category_662 {
 		type = "trade_items";
 		buy[] ={6000,"Coins"};
 		sell[] ={3000,"Coins"};
-        };
-    class ItemComboLock {
+	};
+	class ItemComboLock {
 		type = "trade_items";
 		buy[] ={10000,"Coins"};
 		sell[] ={5000,"Coins"};
 	};
-      class ItemLockbox {
+	class ItemLockbox {
 	    type = "trade_items";
 		buy[] ={40000,"Coins"};
 		sell[] ={20000,"Coins"};
@@ -64,28 +69,103 @@ class Category_662 {
 		buy[] ={60000,"Coins"};
 		sell[] ={10000,"Coins"};
 	};
-		class ChainSawB {
+	class ChainSawB {
 	    type = "trade_weapons";
 		buy[] ={60000,"Coins"};
 		sell[] ={10000,"Coins"};
 	};
-		 class ChainSawG {
+	class ChainSawG {
 	    type = "trade_weapons";
 		buy[] ={60000,"Coins"};
 		sell[] ={10000,"Coins"};
 	};
-		 class ChainSawP {
+	class ChainSawP {
 	    type = "trade_weapons";
 		buy[] ={60000,"Coins"};
 		sell[] ={10000,"Coins"};
 	};
-		 class ChainSawR {
+	 class ChainSawR {
 	    type = "trade_weapons";
 		buy[] ={60000,"Coins"};
 		sell[] ={10000,"Coins"};
 	};
 	 class ItemMixOil {
 	    type = "trade_items";
+		buy[] ={5000,"Coins"};
+		sell[] ={2500,"Coins"};
+	};
+	class fuel_pump_kit {
+	    type = "trade_weapons";
+		buy[] ={60000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class metal_panel_kit {
+	    type = "trade_weapons";
+		buy[] ={60000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class deer_stand_kit {
+	    type = "trade_weapons";
+		buy[] ={60000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class forest_large_net_kit {
+	    type = "trade_weapons";
+		buy[] ={60000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class light_pole_kit {
+	    type = "trade_weapons";
+		buy[] ={60000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class ItemFireBarrel_kit {
+	    type = "trade_weapons";
+		buy[] ={60000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class wooden_shed_kit {
+	    type = "trade_weapons";
+		buy[] ={60000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class m240_nest_kit {
+	    type = "trade_weapons";
+		buy[] ={150000,"Coins"};
+		sell[] ={75000,"Coins"};
+	};
+	class workbench_kit {
+	    type = "trade_weapons";
+		buy[] ={10000,"Coins"};
+		sell[] ={5000,"Coins"};
+	};
+	class ItemScaffoldingKit {
+	    type = "trade_weapons";
+		buy[] ={50000,"Coins"};
+		sell[] ={25000,"Coins"};
+	};
+	class wood_ramp_kit {
+	    type = "trade_weapons";
+		buy[] ={50000,"Coins"};
+		sell[] ={25000,"Coins"};
+	};
+	class MortarBucket {
+	    type = "trade_weapons";
+		buy[] ={500,"Coins"};
+		sell[] ={250,"Coins"};
+	};
+	class CinderBlocks {
+	    type = "trade_weapons";
+		buy[] ={1000,"Coins"};
+		sell[] ={500,"Coins"};
+	};
+	class metal_floor_kit  {
+	    type = "trade_weapons";
+		buy[] ={5000,"Coins"};
+		sell[] ={2500,"Coins"};
+	};
+	class sun_shade_kit {
+	    type = "trade_weapons";
 		buy[] ={5000,"Coins"};
 		sell[] ={2500,"Coins"};
 	};
@@ -156,38 +236,113 @@ class Category_508 {
 		buy[] ={10000,"Coins"};
 		sell[] ={5000,"Coins"};
 	};  
-     class ItemLockbox {
+	class ItemLockbox {
 	        type = "trade_items";
 		buy[] ={40000,"Coins"};
 		sell[] ={20000,"Coins"};
 	};
-		 class ChainSaw {
+	 class ChainSaw {
 	    type = "trade_weapons";
 		buy[] ={20000,"Coins"};
 		sell[] ={10000,"Coins"};
 	};
-		 class ChainSawB {
+	class ChainSawB {
 	    type = "trade_weapons";
 		buy[] ={20000,"Coins"};
 		sell[] ={10000,"Coins"};
 	};
-		 class ChainSawG {
+	class ChainSawG {
 	    type = "trade_weapons";
 		buy[] ={20000,"Coins"};
 		sell[] ={10000,"Coins"};
 	};
-		 class ChainSawP {
+	class ChainSawP {
 	    type = "trade_weapons";
 		buy[] ={20000,"Coins"};
 		sell[] ={10000,"Coins"};
 	};
-		 class ChainSawR {
+	class ChainSawR {
 	    type = "trade_weapons";
 		buy[] ={20000,"Coins"};
 		sell[] ={10000,"Coins"};
 	};
-	 class ItemMixOil {
+	class ItemMixOil {
 	    type = "trade_items";
+		buy[] ={5000,"Coins"};
+		sell[] ={2500,"Coins"};
+	};
+	class fuel_pump_kit {
+	    type = "trade_weapons";
+		buy[] ={60000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class metal_panel_kit {
+	    type = "trade_weapons";
+		buy[] ={60000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class deer_stand_kit {
+	    type = "trade_weapons";
+		buy[] ={60000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class forest_large_net_kit {
+	    type = "trade_weapons";
+		buy[] ={60000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class light_pole_kit {
+	    type = "trade_weapons";
+		buy[] ={60000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class ItemFireBarrel_kit {
+	    type = "trade_weapons";
+		buy[] ={60000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class wooden_shed_kit {
+	    type = "trade_weapons";
+		buy[] ={60000,"Coins"};
+		sell[] ={10000,"Coins"};
+	};
+	class m240_nest_kit {
+	    type = "trade_weapons";
+		buy[] ={150000,"Coins"};
+		sell[] ={75000,"Coins"};
+	};
+	class workbench_kit {
+	    type = "trade_weapons";
+		buy[] ={10000,"Coins"};
+		sell[] ={5000,"Coins"};
+	};
+	class ItemScaffoldingKit {
+	    type = "trade_weapons";
+		buy[] ={50000,"Coins"};
+		sell[] ={25000,"Coins"};
+	};
+	class wood_ramp_kit {
+	    type = "trade_weapons";
+		buy[] ={50000,"Coins"};
+		sell[] ={25000,"Coins"};
+	};
+	class MortarBucket {
+	    type = "trade_weapons";
+		buy[] ={500,"Coins"};
+		sell[] ={250,"Coins"};
+	};
+	class CinderBlocks {
+	    type = "trade_weapons";
+		buy[] ={1000,"Coins"};
+		sell[] ={500,"Coins"};
+	};
+	class metal_floor_kit {
+	    type = "trade_weapons";
+		buy[] ={5000,"Coins"};
+		sell[] ={2500,"Coins"};
+	};
+	class sun_shade_kit {
+	    type = "trade_weapons";
 		buy[] ={5000,"Coins"};
 		sell[] ={2500,"Coins"};
 	};
