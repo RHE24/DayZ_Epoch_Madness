4bf199368ec591e6b42c5d68ff33cd0ac9d96f21
Michael Gallagher <michael@mikesgallagher.com>
2017-03-02 19:15:41 -0700

    Changed Duplicate Typo
    Duplicate Typo

--- DayZ_OverEpoch_1.Chernarus/ZSC/config/Category/HeroWeapons.hpp
+++ DayZ_OverEpoch_1.Chernarus/ZSC/config/Category/HeroWeapons.hpp
@@ -33,6 +33,5 @@ class M249_EP1_DZ {type = "trade_weapons";buy[] ={60000,"Coins"};sell[] ={17500,
 class M249_m145_EP1 {type = "trade_weapons";buy[] ={65000,"Coins"};sell[] ={20000,"Coins"};};
 class M249_m145_EP1_DZE {type = "trade_weapons";buy[] ={65000,"Coins"};sell[] ={20000,"Coins"};};
 class M249_TWS_EP1 {type = "trade_weapons";buy[] ={65000,"Coins"};sell[] ={20000,"Coins"};};
-class Mk_48_DZ {type = "trade_weapons";buy[] ={65000,"Coins"};sell[] ={20000,"Coins"};};
 class Pecheneg_DZ {type = "trade_weapons";buy[] ={62500,"Coins"};sell[] ={20000,"Coins"};};
 };
